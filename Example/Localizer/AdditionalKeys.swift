dcd8a662b186871630962e1d648df64d01af6d98
Danil Lomaev <d.lomaev@vk.team>
2024-04-01 22:59:19 +0300

    fix

--- Example/Localizer/AdditionalKeys.swift
+++
@@ -1,8 +0,0 @@
-// additional keys that should be localized, but not present in code or UI files
-
-// MARK: - Remote notifications
-// "P.OA".localized
-// "P.OF".localized
-// "P.RO".localized
-// "P.CR".localized
-// "P.OM".localized
